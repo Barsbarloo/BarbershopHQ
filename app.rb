b15e9dd062484b735f73437cb81832313f5ee46e
Barsbarloo <lootanke@gmail.com>
2021-06-03 15:35:31 +0600

    Display error on failed validation

--- app.rb
+++ app.rb
@@ -29,8 +29,9 @@ end
 post '/visit' do
 
 	c = Clients.new params[:client]
-	c.save
-
-	erb "<h2>Спасибо, вы записались!</h2>"
-
+	if c.save
+		erb "<h2>Спасибо, вы записались!</h2>"
+	else 
+		erb "<h2>Ошибка</h2>"
+	end	
 end
